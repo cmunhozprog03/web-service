fbb08a2196de98218e79b0ca02c6672ba51e2edc
cmunhozprog03 <cmunhozprog03@hotmail.com>
2021-07-04 12:51:29 -0300

    Category completed

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,19 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateCategory;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    protected $repository;
+
+    public function __construct(Category $category)
+    {
+        $this->category = $category;
+    }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Category $category, Request $request)
+    public function index(Request $request)
     {
-        $categories = $category->getResults($request->name);
+        $categories = $this->category->getResults($request->name);
 
         return response()->json($categories);
     }
@@ -26,9 +33,14 @@ class CategoryController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreUpdateCategory $request)
     {
-        //
+        $category = $this->category->create($request->all());
+
+        return response()->json([
+            'msg' => 'Registro cadastrado com sucessso',
+            'view' => $category
+        ], 201);
     }
 
     /**
@@ -39,10 +51,9 @@ class CategoryController extends Controller
      */
     public function show($id)
     {
-        $category = Category::find($id);
 
-        if(!$category)
-            return response()->json('message', 'Não há registro');
+        if(!$category =$this->category->find($id))
+            return response()->json(['error' => 'Not find'], 404);
 
         return response()->json($category);
     }
@@ -54,9 +65,16 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(StoreUpdateCategory $request, $id)
     {
-        //
+       $category = $this->category->find($id);
+
+       if(!$category)
+        return response()->json(['error' => 'Not find'], 404);
+
+       $category->update($request->all());
+
+        return response()->json($category);
     }
 
     /**
@@ -67,6 +85,15 @@ class CategoryController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $category = $this->category->find($id);
+        if(!$category)
+        return response()->json(['error' => 'Not find'], 404);
+
+        $category->delete();
+
+        return response()->json([
+            'msg' => 'registro excluído com sucesso!',
+
+        ], 204);
     }
 }
